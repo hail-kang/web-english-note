e9138f76390b7637796e313c990b702d87aff903
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-02 23:16:24 +0900

    react-router 적용 중, 로그인 페이지 완료

--- fe_src/components/login.js
+++ fe_src/components/login.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Row,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 class Login extends React.Component {
   render() {
-    return <Container>Boot Strap</Container>;
+    return (
+      <Container style={{ marginTop: "200px" }}>
+        <Row>
+          <Col md={{ offset: 4, span: 4 }} xs={{ offset: 2, span: 8 }}>
+            <Form>
+              <InputGroup>
+                <FormControl
+                  type="text"
+                  placeholder="이름을 입력하시오"
+                ></FormControl>
+                <Button variant="outline-secondary">확인</Button>
+              </InputGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
--- fe_src/index.js
+++ fe_src/index.js
@@ -2,14 +2,31 @@ import "./index.scss";
 
 import React from "react";
 import { render } from "react-dom";
-import { Router, Route, hashHistory } from "react-router";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import App from "./components/App";
 import Login from "./components/Login";
 
 render(
-  <Router>
-    <Route path="/" component={App} />
-  </Router>,
+  <BrowserRouter>
+    {/* <nav>
+      <ul>
+        <li>
+          <Link to="/appv">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Login</Link>
+        </li>
+      </ul>
+    </nav> */}
+    <Switch>
+      <Route path="/">
+        <Login />
+      </Route>
+      <Route path="/appv">
+        <App />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
